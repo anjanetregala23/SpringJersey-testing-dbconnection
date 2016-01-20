ce7abaf37136225cdb0333034134815e454096cf
An Janet Regala <janetregala23@gmail.com>
2016-01-20 08:49:59 +0800

    update User  and PaymentService class
    add value for the string username in the User class then add a function
    to the PaymentService class

--- src/main/java/com/ParkU/au/PaymentService.java
+++ src/main/java/com/ParkU/au/PaymentService.java
@@ -1,63 +1,50 @@
 package com.ParkU.au;
 
-
-import java.net.UnknownHostException;
-import java.util.Collection;
-import java.util.Date;
-import java.util.LinkedList;
-import java.util.List;
-
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
-import org.springframework.data.mongodb.core.MongoOperations;
-import org.springframework.data.mongodb.core.query.Criteria;
-import org.springframework.data.mongodb.core.query.Query;
-import org.springframework.data.mongodb.core.query.Update;
 import org.springframework.stereotype.Component;
 
-import com.ParkU.classs.Employee;
-import com.jersey.pojo.ConnectMongoDB;
 import com.jersey.pojo.User;
-import com.mongodb.BasicDBObject;
-import com.mongodb.DB;
-import com.mongodb.DBCollection;
-import com.mongodb.DBCursor;
-import com.mongodb.MongoClient;
-import com.mongodb.MongoException;
 import com.springjersey.transaction.TransactionBo;
 
 @Component
 @Path("/payment")
-@Produces(MediaType.APPLICATION_JSON)
 public class PaymentService {
 
 	@Autowired
 	TransactionBo transactionBo;
 
-    @GET
+	@GET
 	@Path("/testing")
-	public Response savePayment() {
+	@Produces(MediaType.APPLICATION_JSON)
+	public User savePayment() {
+
+		// String result = transactionBo.save();
+		// return Response.status(200).entity(result).build();
+		User user = new User();
+		user.setUserName("anjanetregala23");
+		return user;
+	}
 	
-		 String result = transactionBo.save();
-		 return Response.status(200).entity(result).build();
-		//User userName = new User();
-		//return userName;
-	}	
-    
-    public ConnectMongoDB connectDB(){
-    	
-    	ConnectMongoDB conndb = new ConnectMongoDB();
-    	return conndb;
-    }
+	@POST
+	@Path("/post")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response createUserInJSON(User user){
+		String result = "USER saved"+user;
+		return Response.status(200).entity(result).build();
+	}
 	
-	
-	
+
+	/*public ConnectMongoDB connectDB() {
+
+		ConnectMongoDB conndb = new ConnectMongoDB();
+		return conndb;
+	}*/
 
 }
--- src/main/java/com/jersey/pojo/User.java
+++ src/main/java/com/jersey/pojo/User.java
@@ -1,5 +1,7 @@
 package com.jersey.pojo;
 
+import com.sun.xml.bind.annotation.OverrideAnnotationOf;
+
 public class User {
 	private String userName;
 
@@ -10,5 +12,12 @@ public class User {
 	public void setUserName(String userName) {
 		this.userName = userName;
 	}
+	
+	@Override
+	public String toString(){
+		return "User [username="+userName+"]";
+	}
+	
+	
 	
 }
